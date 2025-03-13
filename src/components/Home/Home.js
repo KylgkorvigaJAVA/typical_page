a67ac10b93426639e305bb0c3814730d1d70f68b
Kaspar <kylgorvigajava@gmail.com>
2025-03-13 14:16:05 +0200

    added component folders

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import Card from "../UI/Card";
+import './Home.css';
+
+const Home = () => {
+    return (
+        <Card className="home">
+            <h1>Welcome back!</h1>
+        </Card>
+    )
+}
